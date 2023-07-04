2a77e498cd87e06e331449b9f9a77f26eef76c4a
Joelinseoul <gynpark@gmail.com>
2023-07-04 21:32:29 +0900

    spring 예제 프로젝트 최초 파일 추가

---
+++ src/main/java/com/joelinseoul/spring/ex/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.joelinseoul.spring.ex.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloworld() {
+		return "hello World!!";
+	}
+
+}
